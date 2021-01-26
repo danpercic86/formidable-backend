b1220464a1f480a12729cf53c2007299806480e3
Dan Percic <danpercic86@gmail.com>
2021-01-26 14:15:24 +0200

    remove duplicate files

--- createsuperuser.py
+++ createsuperuser.py
@@ -1,12 +1,19 @@
-from django.contrib.auth import get_user_model
+import os
+
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings.dev")
+django.setup()
+
 from django.db.utils import IntegrityError
+from administration.models import User
 
 USERNAME = 'admin'
 EMAIL = 'admin@example.com'
 PASSWORD = 'admin'
 
 try:
-    get_user_model().objects.create_superuser(USERNAME, EMAIL, PASSWORD)
+    User.objects.create_superuser(USERNAME, EMAIL, PASSWORD)
 except IntegrityError:
     print(f"User '{USERNAME} <{EMAIL}>' already exists")
 else:
